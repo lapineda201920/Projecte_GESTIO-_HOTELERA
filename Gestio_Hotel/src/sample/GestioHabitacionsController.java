439ae2a1436f0786ceecb2bcb21f09c9e72eeb38
lapineda201920 <lapinedalapineda@gmail.com>
2020-04-24 16:59:37 +0200

    Gestió Habitacions

--- Gestio_Hotel/src/sample/GestioHabitacionsController.java
+++ Gestio_Hotel/src/sample/GestioHabitacionsController.java
@@ -1,4 +1,46 @@
 package sample;
 
-public class GestioHabitacionsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class GestioHabitacionsController implements Initializable {
+
+    @FXML
+    private TextField numero, pis, descripcio, caracteristiques, preu, estat, tipo;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources){
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection ("jdbc:mysql://172.17.0.1:3306/Hotel","root", "claveroot");
+
+            // PREPAREM LA CONSULTA
+            Statement s = conexion.createStatement();
+            ResultSet rs = s.executeQuery ("select * from Usuaris");
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+
+            }
+
+            conexion.close();
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
 }
