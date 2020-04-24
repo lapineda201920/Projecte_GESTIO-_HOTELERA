a86646ac171caf902ff8f2c59f2d35d429be22f6
JoelP-student <joel.plaza@inslapineda.cat>
2020-04-24 17:23:40 +0200

    Modificacions varies Gestio Clients

---
+++ Gestio_Hotel/src/sample/GestioClientsController.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.*;
+
+public class GestioClientsController{
+
+    @FXML
+    private ListView list_todo;
+
+
+
+}
